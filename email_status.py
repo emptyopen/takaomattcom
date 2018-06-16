c95aee6f4be34daa027c695f20ef79c99c12abd8
emptyopen <takaomatt@gmail.com>
2018-06-15 18:54:46 -0700

    connecting user.db to emails

--- email_status.py
+++ email_status.py
@@ -36,7 +36,7 @@ class EmailStatus(object):
 
         with open(self.par_path + '/auth/takaomattpython.txt') as f:
             email_password = f.read()
-	
+
         with open(self.par_path + '/auth/robinhood.txt') as f:
             data = f.read().split('\n')
             RH_username = data[0]
@@ -46,16 +46,15 @@ class EmailStatus(object):
         self.my_trader.login(username=RH_username, password=RH_password)
         self.equity = self.my_trader.equity()
 
-
         self.server = smtplib.SMTP('smtp.gmail.com', 587)
         self.server.starttls()
         self.server.login('takaomattpython@gmail.com', email_password)
 
-        # frequency: daily, weekly, monthly, quarterly, never
-        self.users = {'matt':['Matt', 0.6187, 'takaomatt@gmail.com', 'daily'],
-                      'tim':['Tim', 0.0836, 'takaotim@gmail.com', 'weekly'],
-                      'mazy':['Mazy', 0.0431, 'mazyyap@gmail.com', 'weekly'],
-                      'andrew':['Andrew', 0.2545, 'takaoandrew@gmail.com', 'weekly']}
+        conn_users = sqlite3.connect(self.home_path + 'users.db')
+        cursor_users = conn_users.cursor()
+        self.users = cursor_users.execute("SELECT * FROM user;").fetchall()
+        associate = {'takaomatt@gmail.com':'Matt', 'takaotim@gmail.com':'Tim', 'mazyyap@gmail.com':'Mazy', 'takaoandrew@gmail.com':'Andrew'}
+        self.users = {str(x[2]):[associate[x[2]], x[4], str(x[5])] for x in self.users}
 
 
     def create_daily_historical_value_plot(self, ownership=1):
@@ -104,12 +103,17 @@ class EmailStatus(object):
             plt.close()
 
 
-    def send_email(self, name, percent_owned, email_address):
+    def send_email(self, user):
+
+        email_address = user
+        name = self.users[user][0]
+        percent_owned = self.users[user][1]
+        frequency = self.users[user][2]
 
         msg = MIMEMultipart()
         msg['Date'] = formatdate(localtime=True)
         now = dt.datetime.now()
-        msg['Subject'] = 'Your Portfolio Status for {}-{}-{}'.format(now.year, now.month, now.day)
+        msg['Subject'] = 'Your {} Portfolio Status ({}-{}-{})'.format(frequency, now.year, now.month, now.day)
 
         THIS_DIR = os.path.dirname(os.path.abspath(__file__))
         j2_env = Environment(loader=FileSystemLoader(THIS_DIR), trim_blocks=True)
@@ -127,10 +131,20 @@ class EmailStatus(object):
         self.server.quit()
 
 
-    def send_emails(self, force=False):
+    def send_status_emails(self, force=False):
+
+        # debug
+        if True:
+            self.users = {'takaomatt@gmail.com':['Matt', 0.6187, 'Daily'],
+                          'takaotim@gmail.com':['Tim', 0.0836, 'Weekly'],
+                          'mazyyap@gmail.com':['Mazy', 0.0431, 'Weekly'],
+                          'takaoandrew@gmail.com':['Andrew', 0.2545, 'Weekly']}
 
         if False:
-            self.users = {'matt':['Matt', 0.6187, 'takaomatt@gmail.com', 'daily']}
+            self.users = {'takaomatt@gmail.com':['Matt', 0.6187, 'Daily']}
+
+        if False:
+            self.users = {}
 
         for user in self.users:
             self.create_daily_historical_value_plot(self.users[user][1])
@@ -138,21 +152,22 @@ class EmailStatus(object):
             # weekly = every monday
             # monthly = first monday of every month
             # quarterly = first monday of January, April, July, October
-            send_if = ['daily']
+            send_if = ['Daily']
             today = dt.date.today()
             if today.weekday() == 0:
-                send_if.append('weekly')
+                send_if.append('Weekly')
             if today.day <= 7 and today.weekday() == 0:
-                send_if.append('monthly')
+                send_if.append('Monthly')
             if today.month in [1, 4, 7, 10] and today.day <= 7 and today.weekday() == 0:
-                send_if.append('quarterly')
-            if contents[3] in send_if or force:
-                print('sending to {}: {}'.format(self.users[user][0], self.users[user][1]))
-                self.send_email(contents[0], contents[1], contents[2])
+                send_if.append('Quarterly')
+            if self.users[user][2] in send_if or force:
+                print('sending to {}'.format(user))
+                self.send_email(user)
             if os.path.isfile('static/img/daily_historical_portfolio.png'):
                 os.remove('static/img/daily_historical_portfolio.png')
 
 
 
+
 E = EmailStatus()
-E.send_emails()
+E.send_status_emails()
